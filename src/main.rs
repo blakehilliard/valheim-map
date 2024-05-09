813607d2894cd6f0841521297800917005b5484b
blakehilliard <blakesto@gmail.com>
2024-05-08 23:11:06 -0500

    add some building coordinates

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,29 @@
+#[derive(Debug)]
+struct Building {
+    name: String,
+    x: i64,
+    y: i64,
+}
+
+impl Building {
+    pub fn new(name: &str, x: i64, y: i64) -> Self {
+        Self {
+            name: String::from(name),
+            x, y,
+        }
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    // Define buildings
+    let buildings = vec![
+        Building::new("Hrogden's Hrarbor", 0, -130),
+        Building::new("Timberhold", 77, -130),
+        Building::new("Skjor's Landing", -43, -422),
+    ];
+
+    // Draw
+    for building in buildings {
+        println!("TODO: Draw {:?}", building)
+    }
 }
